fb9a420645e5b9e7987b95cf15a1d1c9042b0856
musfiqur521 <musfiqur521@gmail.com>
2023-11-13 12:00:07 +0600

    categories_search

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -8,8 +8,16 @@ use Illuminate\Support\Facades\Validator;
 use App\Models\Category;
 class CategoryController extends Controller
 {
-    public function index(){
-        $categories = Category::latest()->paginate(10);
+    public function index(Request $request){
+        $categories = Category::latest();
+
+        if(!empty($request->get('keyword'))){
+            $categories = $categories->where('name','like','%'.$request->get('keyword').'%');
+
+        }
+
+        $categories = $categories->paginate(10);
+        
         return view('admin.category.list',compact('categories'));
     }
 
